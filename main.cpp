f9fee2d91aa3bd62af377201d15501a72a7226f2
Ivar Stangeby <istangeby@gmail.com>
2025-06-23 17:37:22 +0200

    Remove crud.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include "src/vec3.h"
 
 #include <iostream>
 int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    Vec3 vec = Vec3();
-    std::cout << vec << "\n";
     for (int i = 1; i <= 5; i++) {
         std::cout << "i = " << i << std::endl;
     }
